dcd72f15d513937f812d1a54f855c1a1e08ebf47
Tosh5 <75401420+Tosh5@users.noreply.github.com>
2021-12-31 23:50:42 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){   
+    int A,B;
+    string op;
+
+    cin >> A >> B;
+    cin >> op;
+
+    if(op == "+"){
+        cout << A+B << endl;
+    }else if(op == "-"){
+        cout << A-B << endl;
+    }else if(op == "*"){
+        cout << A*B << endl;
+    }else if(op == "/"){
+        cout << A/B << endl;
+    }else{
+        cout << "error" << endl;
+    }
+
+    //std::cout << "Hello World!" << std::endl;
+    return 0;
+    
+}
